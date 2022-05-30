281ef8ad1d29ddc203d6d9689b27e9f6f02bbe2d
dev-bong <qhddnjswo011@gmail.com>
2022-05-30 15:38:49 +0900

    :art: 코드 포맷팅 (black)

--- src/chat_client.py
+++ src/chat_client.py
@@ -8,61 +8,65 @@ import sys
 IP = "server ip"
 PORT = 20205
 
-class MessageManager():# 서버와 연결 및 메시지 송수신 처리 역할
+
+class MessageManager:  # 서버와 연결 및 메시지 송수신 처리 역할
     def __init__(self):
         self.client_socket = None
         self.con_state = 1
 
-    def connect_to_server(self): # 서버에 연결
+    def connect_to_server(self):  # 서버에 연결
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((IP, PORT))
 
-    def send_msg(self, msg): # 서버로 메시지 전송
+    def send_msg(self, msg):  # 서버로 메시지 전송
         print(f"send msg to server : {msg}")
         msg = msg.encode(encoding="utf-8")
         self.client_socket.sendall(msg)
 
-    def receive_msg_check(self): # 메시지 받으면서 연결상태? 체크
+    def receive_msg_check(self):  # 메시지 받으면서 연결상태? 체크
         try:
             msg = self.client_socket.recv(1024).decode()
         except:
             return 0
-        
+
         print(f"receive msg from server : {msg}")
         return msg
 
-    def disconnect_from_server(self): # 서버와 연결 해제
+    def disconnect_from_server(self):  # 서버와 연결 해제
         self.client_socket.close()
 
-class ClientWindow():
+
+class ClientWindow:
     def __init__(self):
-        self.window = None                  # 클라이언트 전체 윈도우
-        self.chat_input_box = None          # 채팅 입력 박스
-        self.send_button = None             # 전송 버튼
-        self.chat_contents_field = None     # 채팅 내용이 보여지는 필드
-        self.bold_font = None               # 굵은 글씨 폰트 (닉네임에 사용)
-        self.colors = {                     # 색상들. key는 색상 코드 (닉네임에 사용)
-            1 : "dark orchid",
-            2 : "turquoise",
-            3 : "dark salmon",
-            4 : "pink",
-            5 : "DodgerBlue4",
-            6 : "SpringGreen3"
-        }                                   # 일단 6개.. 추후 더 추가하도록
+        self.window = None  # 클라이언트 전체 윈도우
+        self.chat_input_box = None  # 채팅 입력 박스
+        self.send_button = None  # 전송 버튼
+        self.chat_contents_field = None  # 채팅 내용이 보여지는 필드
+        self.bold_font = None  # 굵은 글씨 폰트 (닉네임에 사용)
+        self.colors = {  # 색상들. key는 색상 코드 (닉네임에 사용)
+            1: "dark orchid",
+            2: "turquoise",
+            3: "dark salmon",
+            4: "pink",
+            5: "DodgerBlue4",
+            6: "SpringGreen3",
+        }  # 일단 6개.. 추후 더 추가하도록
         self.manager = None
 
-    def set_window(self): # 윈도우 세팅
+    def set_window(self):  # 윈도우 세팅
         # 전체 윈도우 생성 및 설정
         self.window = tk.Tk()
         self.window.title("hello!")
-        self.window.geometry("400x400+100+100") # 긴급탈출 넣을거면 400x450으로
-        self.window.resizable(False, False) # 창 크기 조절 가능 여부 (상하, 좌우)
+        self.window.geometry("400x400+100+100")  # 긴급탈출 넣을거면 400x450으로
+        self.window.resizable(False, False)  # 창 크기 조절 가능 여부 (상하, 좌우)
 
         # 기타 위젯 생성 및 설정
-        self.chat_contents_field = scrolledtext.ScrolledText(self.window, width = 50, height = 27)
+        self.chat_contents_field = scrolledtext.ScrolledText(
+            self.window, width=50, height=27
+        )
         self.chat_input_box = tk.Entry(self.window, width=40)
         self.send_button = tk.Button(self.window, width=10, text="전송")
-        self.bold_font = tk_font.Font(weight="bold") # 닉네임은 굵게
+        self.bold_font = tk_font.Font(weight="bold")  # 닉네임은 굵게
 
         # 위젯 배치
         self.chat_contents_field.place(x=20, y=0)
@@ -70,29 +74,35 @@ class ClientWindow():
         self.send_button.place(x=310, y=370)
 
         # 위젯 - 이벤트 묶기
-        self.send_button.bind("<Button-1>", self.press_send_button)             # 전송 버튼을 마우스 좌클릭하면 send_msg 실행하도록 (<Button-1> : 마우스 좌클릭)
-        self.chat_input_box.bind("<Return>", self.press_send_button)            # input box에 Enter키가 입력되면 send_msg 실행하도록 (<Return> : Enter 버튼 입력)
-        self.chat_contents_field.bind("<Key>", lambda e: "break")               # 채팅필드 키 입력 불가
-        self.chat_contents_field.bind("<Button-1>", lambda e: "break")          # 채팅필드 클릭 불가
+        self.send_button.bind(
+            "<Button-1>", self.press_send_button
+        )  # 전송 버튼을 마우스 좌클릭하면 send_msg 실행하도록 (<Button-1> : 마우스 좌클릭)
+        self.chat_input_box.bind(
+            "<Return>", self.press_send_button
+        )  # input box에 Enter키가 입력되면 send_msg 실행하도록 (<Return> : Enter 버튼 입력)
+        self.chat_contents_field.bind("<Key>", lambda e: "break")  # 채팅필드 키 입력 불가
+        self.chat_contents_field.bind("<Button-1>", lambda e: "break")  # 채팅필드 클릭 불가
 
         # 초기 메시지
-        self.chat_contents_field.insert("insert","닉네임을 입력하세요\n")
+        self.chat_contents_field.insert("insert", "닉네임을 입력하세요\n")
 
         # 창닫기 버튼 누를 시 exit_window 실행
         self.window.protocol("WM_DELETE_WINDOW", self.exit_window)
 
         # 색상 태그들 생성
         for color_code in self.colors:
-            self.chat_contents_field.tag_config(str(color_code), foreground=self.colors[color_code], font=self.bold_font)
+            self.chat_contents_field.tag_config(
+                str(color_code), foreground=self.colors[color_code], font=self.bold_font
+            )
 
-    def exit_window(self): # 윈도우 나가기. 서버에 종료 알리고 소켓 닫기
+    def exit_window(self):  # 윈도우 나가기. 서버에 종료 알리고 소켓 닫기
         exit_msg = "/exit"
         self.manager.send_msg(exit_msg)
         self.manager.disconnect_from_server()
         self.window.destroy()
         sys.exit(1)
 
-    def press_send_button(self, e): # 입력받은 메시지 서버로 전송 (e는 이벤트에 대한 값이 들어옴)
+    def press_send_button(self, e):  # 입력받은 메시지 서버로 전송 (e는 이벤트에 대한 값이 들어옴)
         # input box에서 메시지 받은 후 input box 초기화
         msg = self.chat_input_box.get()
         if not msg:
@@ -102,17 +112,17 @@ class ClientWindow():
 
         self.manager.send_msg(msg)
 
-    def run(self): # 채팅 클라이언트(메시지 매니저 + 윈도우) 실행
+    def run(self):  # 채팅 클라이언트(메시지 매니저 + 윈도우) 실행
         self.manager = MessageManager()
         self.manager.connect_to_server()
         self.set_window()
 
         th = threading.Thread(target=self.chat_stream_to_field)
         th.start()
-        
+
         self.window.mainloop()
 
-    def chat_stream_to_field(self): # 서버로부터 계속해서 메시지 받으며 채팅창에 출력
+    def chat_stream_to_field(self):  # 서버로부터 계속해서 메시지 받으며 채팅창에 출력
         while True:
             msg = self.manager.receive_msg_check()
             if not msg:
@@ -129,23 +139,29 @@ class ClientWindow():
                 cid = None
 
             # chat contents field에 메시지 출력
-            self.chat_contents_field.insert("end",msg) # insert vs end
+            self.chat_contents_field.insert("end", msg)  # insert vs end
             self.chat_contents_field.see("end")
 
             # 닉네임에 색 입히기
             if color_code:
-                st = self.chat_contents_field.search(cid, "end-2l", "end") # 끝에서 2라인정도? 클라이언트 닉네임 검색
+                st = self.chat_contents_field.search(
+                    cid, "end-2l", "end"
+                )  # 끝에서 2라인정도? 클라이언트 닉네임 검색
                 while st:
                     st_e = st.split(".")[1]
                     l = len(cid)
                     end = st + f"+{l}c"
-                    if st_e == "0": # 각 라인의 처음에서 검색된게 아니면 색상 입히지 않음 (채팅 내용에서 언급되는 닉네임에 색상 X)
+                    if (
+                        st_e == "0"
+                    ):  # 각 라인의 처음에서 검색된게 아니면 색상 입히지 않음 (채팅 내용에서 언급되는 닉네임에 색상 X)
                         self.chat_contents_field.tag_add(color_code, st, end)
                     st = self.chat_contents_field.search(cid, end, "end")
+
 
 def client_start():
     client_window = ClientWindow()
     client_window.run()
+
 
 if __name__ == "__main__":
     client_start()
--- src/chat_server.py
+++ src/chat_server.py
@@ -4,41 +4,42 @@ import threading
 IP = ""
 PORT = 20205
 
-class Room: # 채팅방
+
+class Room:  # 채팅방
     def __init__(self):
         self.client_list = []
         self.server_socket = None
-        self.usable_colors = [1,2,3,4,5,6] # 사용 가능한 색상코드들
-        self.colors_in_use = [] # 사용중인 색상코드들
+        self.usable_colors = [1, 2, 3, 4, 5, 6]  # 사용 가능한 색상코드들
+        self.colors_in_use = []  # 사용중인 색상코드들
 
-    def ready_to_connect(self): # 서버 소켓 생성 및 bind, listen
+    def ready_to_connect(self):  # 서버 소켓 생성 및 bind, listen
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((IP, PORT))
         self.server_socket.listen()
         print("Start server..")
-    
-    def enter_client(self, c): # 클라이언트 입장
+
+    def enter_client(self, c):  # 클라이언트 입장
         self.client_list.append(c)
 
-    def leave_client(self, c): # 클라이언트 퇴장
+    def leave_client(self, c):  # 클라이언트 퇴장
         self.client_list.remove(c)
 
-    def push_msg_to_room(self, msg): # 채팅방에 있는 모든 사람한테 메시지 전송
+    def push_msg_to_room(self, msg):  # 채팅방에 있는 모든 사람한테 메시지 전송
         for c in self.client_list:
             c.send_msg(msg)
 
-    def give_color(self, nickname): # 닉네임에 색상코드 부여
+    def give_color(self, nickname):  # 닉네임에 색상코드 부여
         color = self.usable_colors.pop(0)
         self.colors_in_use.append(color)
-        return str(color) + "|" + nickname # bong -> 2|bong 이런식으로.. | 앞에 숫자가 색상코드
+        return str(color) + "|" + nickname  # bong -> 2|bong 이런식으로.. | 앞에 숫자가 색상코드
 
-    def take_color(self, nickname): # 닉네임 색상코드 반환
+    def take_color(self, nickname):  # 닉네임 색상코드 반환
         color = int(nickname.split("|")[0])
         self.colors_in_use.remove(color)
         self.usable_colors.append(color)
 
-    def run(self): # 채팅 서버 실행
+    def run(self):  # 채팅 서버 실행
         self.ready_to_connect()
 
         while True:
@@ -49,7 +50,8 @@ class Room: # 채팅방
             th = threading.Thread(target=c.receive_msg)
             th.start()
 
-class ClientManager: # 클라이언트, 유저 관리자
+
+class ClientManager:  # 클라이언트, 유저 관리자
     def __init__(self, cm_inform, r):
         soc, addr = cm_inform
         self.nickname = None
@@ -57,53 +59,57 @@ class ClientManager: # 클라이언트, 유저 관리자
         self.room = r
         print(f"Enter {addr}")
 
-    def send_msg(self, msg): # 클라이언트로 메시지 전송
+    def send_msg(self, msg):  # 클라이언트로 메시지 전송
         msg = msg.encode(encoding="utf-8")
         self.listen_socket.sendall(msg)
 
-    def receive_msg_check(self): # 메시지 받으면서 연결상태? 체크
+    def receive_msg_check(self):  # 메시지 받으면서 연결상태? 체크
         try:
             msg = self.listen_socket.recv(1024).decode()
         except:
             return 0
-        
+
         return msg
 
-    def receive_msg(self): # 계속해서 대기하며 클라이언트로부터 메시지 수신
+    def receive_msg(self):  # 계속해서 대기하며 클라이언트로부터 메시지 수신
         self.nickname = self.receive_msg_check()
         print(self.nickname)
-        if self.nickname == "/exit": # nickname을 받는데 실패하거나 nickname을 입력전에 클라이언트를 닫아버린 경우
+        if (
+            self.nickname == "/exit"
+        ):  # nickname을 받는데 실패하거나 nickname을 입력전에 클라이언트를 닫아버린 경우
             print("connection fail")
             self.room.leave_client(self)
-            print("Client :",self.room.client_list)
+            print("Client :", self.room.client_list)
             return
-        msg = self.nickname + '님이 입장하셨습니다'
+        msg = self.nickname + "님이 입장하셨습니다"
         self.room.push_msg_to_room(msg)
 
-        self.nickname = self.room.give_color(self.nickname) # 닉네임에 색상코드 부여
+        self.nickname = self.room.give_color(self.nickname)  # 닉네임에 색상코드 부여
 
         while True:
             msg = self.receive_msg_check()  # 사용자가 전송한 메시지 읽음
             if not msg:
                 self.leave()
                 break
-            elif msg == "/exit": # 종료 메시지이면 종료
+            elif msg == "/exit":  # 종료 메시지이면 종료
                 self.leave()
                 break
             print(msg, type(msg))
-            msg = self.nickname+': '+ msg
-            self.room.push_msg_to_room(msg) # 모든 사용자에 메시지 전송
+            msg = self.nickname + ": " + msg
+            self.room.push_msg_to_room(msg)  # 모든 사용자에 메시지 전송
 
-    def leave(self): # 퇴장
+    def leave(self):  # 퇴장
         self.room.leave_client(self)
-        self.room.take_color(self.nickname) # 퇴장 시 색상코드 반환
-        self.room.push_msg_to_room(self.nickname + '님이 퇴장하셨습니다.')
+        self.room.take_color(self.nickname)  # 퇴장 시 색상코드 반환
+        self.room.push_msg_to_room(self.nickname + "님이 퇴장하셨습니다.")
         self.listen_socket.close()
-        print("Current clients :",self.room.client_list)
+        print("Current clients :", self.room.client_list)
+
 
 def server_start():
     chat_room = Room()
     chat_room.run()
+
 
 if __name__ == "__main__":
     server_start()
